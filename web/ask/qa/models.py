fbdc35c055007ee82ff79a07c1b9dd6abe8a9608
Serge Koksharov <gentoosiastic@gmail.com>
2016-11-11 23:42:59 +0300

    Fixed all bugs

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -12,15 +12,15 @@ class QuestionManager(models.Manager):
 class Question(models.Model):
     title = models.CharField(max_length=250)
     text = models.TextField()
-    added_at = models.DateField()
-    rating = models.IntegerField()
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User)
     likes = models.ManyToManyField(User, related_name='likes_set')
     objects = QuestionManager()
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateField()
+    added_at = models.DateField(auto_now_add=True)
     question = models.OneToOneField(Question)
     author = models.ForeignKey(User)
 
