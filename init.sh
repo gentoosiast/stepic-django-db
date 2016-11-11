ded0c068f7ee696786e10e82a349789ffbd91069
Serge Koksharov <gentoosiastic@gmail.com>
2016-11-11 23:20:27 +0300

    More fixes

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+sudo /etc/init.d/mysql restart
 mysql -u root -e "CREATE USER 'askuser'@'localhost' IDENTIFIED BY 'simplepass'"
 mysql -u root -e "CREATE DATABASE askdb CHARACTER SET utf8 COLLATE utf8_general_ci"
 mysql -u root -e "GRANT ALL PRIVILEGES ON askdb.* TO 'askuser'@'localhost'"
@@ -8,5 +9,4 @@ sudo ln -s /home/box/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/etc/gunicorn.conf /etc/gunicorn.d/django
 sudo /etc/init.d/nginx restart
 sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql restart
 python2 /home/box/web/ask/manage.py syncdb
--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -3,6 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+    def popular(self):
+        return self.order_by('-rating')
+
 class Question(models.Model):
     title = models.CharField(max_length=250)
     text = models.TextField()
@@ -12,15 +18,9 @@ class Question(models.Model):
     likes = models.ManyToManyField(User, related_name='likes_set')
     objects = QuestionManager()
 
-class QuestionManager(models.Manager):
-    def new(self):
-        return self.order_by('-added_at')
-    def popular(self):
-        return self.order_by('-rating')
-
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateField()
-    question = OneToOneField(Question)
+    question = models.OneToOneField(Question)
     author = models.ForeignKey(User)
 
